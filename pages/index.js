0f15e31dc53b752034c51df1458e6dd3bd8c32cc
albenchris <albenchris00@gmail.com>
2021-07-26 17:29:00 -0500

    added first few lines of home with image rendering

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,39 @@ export default function Home() {
         <title>Kiteboard Milwaukee</title>
         <meta name='keywords' content='kiteboard, kiteboarding' />
       </Head>
-      <main>
+      <main id='main-content'>
+
         (Home)
+
+        <div>
+          <a href='/images/3e3127bb1a.png' rel='lightbox[lb207]'>
+            <img
+              src='/images/f971906372.jpg'
+              width='300'
+              height='200'
+              border='0'
+              alt='kiteboarder in the water'
+            />
+          </a>
+
+          <h2>Welcome to Kiteboarding in Milwaukee</h2>
+          <p>
+          We hope you find this site a great resource for kiters of all abilities!
+          Whether you are into kiteboarding, kitesurfing, landboarding, snowkiting, or whatever with a kite to move you around, you are welcome here.
+          Your comments and suggestions will help make this a great site.
+          Just 
+            <a href='contact-section'>
+              contact us here
+            </a>
+          or send us an email directly to
+            <a href='email'>
+              info@adventurekiteboarding.com
+            </a>
+          or call 414-520-1493. Thanks & enjoy!
+          </p>
+        </div>
+
+        <div></div>
       </main>
     </div>
   )
